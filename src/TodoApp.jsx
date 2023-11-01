3a6e34e0f7625c566dca47cc94b05330baffede1
Tejass2712 <140151256+Tejass2712@users.noreply.github.com>
2023-11-01 19:25:44 +0530

    new

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,15 +1,45 @@
+ 
 import React, { useState } from 'react';
 
 function TodoApp() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
+  const [priority, setPriority] = useState('Medium');  
+  const [dueDate, setDueDate] = useState('');  
+  const [category, setCategory] = useState('');  
 
   const addTask = () => {
     if (newTask.trim() !== '') {
-      setTasks([...tasks, newTask]);
+      const task = {
+        text: newTask,
+        priority,
+        dueDate,
+        category,
+        completed: false,  
+      };
+      setTasks([...tasks, task]);
       setNewTask('');
+      setPriority('Medium');
+      setDueDate('');
+      setCategory('');
     }
   };
+
+  const sortByPriority = () => {
+    const sortedTasks = [...tasks].sort((a, b) => {
+      const priorityOrder = { Low: 1, Medium: 2, High: 3 };
+      return priorityOrder[a.priority] - priorityOrder[b.priority];
+    });
+    setTasks(sortedTasks);
+  };
+
+  const sortByDueDate = () => {
+    const sortedTasks = [...tasks].sort((a, b) => {
+      return new Date(a.dueDate) - new Date(b.dueDate);
+    });
+    setTasks(sortedTasks);
+  };
+ 
 
   return (
     <div>
@@ -19,10 +49,37 @@ function TodoApp() {
         value={newTask}
         onChange={(e) => setNewTask(e.target.value)}
       />
+      <select value={priority} onChange={(e) => setPriority(e.target.value)}>
+        <option value="High">High</option>
+        <option value="Medium">Medium</option>
+        <option value="Low">Low</option>
+      </select>
+      <input
+        type="date"
+        value={dueDate}
+        onChange={(e) => setDueDate(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Category"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      />
       <button onClick={addTask}>Add Task</button>
+   
+
+       
       <ul>
         {tasks.map((task, index) => (
-          <li key={index}>{task}</li>
+          <li key={index}>
+            {task.text}
+            {/* <div>Priority: {task.priority}</div> */}
+            <div>Due Date: {task.dueDate}</div>
+            <div>Category: {task.category}</div>
+            {/* <button onClick={() => toggleSelect(index)}>
+              {task.completed ? 'Deselect' : 'Select'}
+            </button> */}
+          </li>
         ))}
       </ul>
     </div>
@@ -30,3 +87,9 @@ function TodoApp() {
 }
 
 export default TodoApp;
+
+
+
+
+
+
